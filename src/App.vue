9a327e18aaf72ed6b3c50e7093a5d07f5f9e23d7
deepthi0395 <82020988+deepthi0395@users.noreply.github.com>
2021-08-05 18:47:07 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="bg-main">
+    <Contents />
+  </div>
+</template>
+
+<script>
+	import Contents from './views/Contents.vue'
+
+	export default {
+	  components: {
+	    Contents,
+	  },
+	};
+</script>
+
---
+++ src/components/BottomMainPart.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class = "bottomcontainer">
+        <div class = "bottomtoppart">
+            <div>
+            <p>No.of.rounds</p>
+            <input class="countinputbox" type = "text" />
+            <i class="fa fa-plus" aria-hidden="true"> </i>
+            </div> 
+            <div>
+            <p>Type Of Rounds </p>
+            <select >
+                <option>Consecutive Rounds </option>
+                <option>Parallel Rounds </option>
+            </select>
+            </div>
+            <div>
+            <p>Type Of Contest </p>
+            <select>
+                <option>Online/Offline Rounds</option>
+                <option>Offline Contest</option>
+                <option>Online Contest</option>
+            </select>
+            </div>
+        
+        
+        </div>
+        <hr class="line-through"/>
+        <div class = "bottomlowerpart">
+            <ListComponent />
+            <Extra/>
+        </div>
+    </div>
+</template>
+
+<script>
+import ListComponent from "./ListComponent.vue"
+import Extra from "./Extra.vue"
+   
+export default {
+    components:{
+    ListComponent,
+    Extra
+    },
+}
+</script>
+
+<style>
+.bottomcontainer{
+    display:flex;
+    flex-direction:column;
+}
+.bottomtoppart{
+    display:flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+}
+.line-through{
+    border:1px solid black;
+    width:100%;
+}
+.bottomlowerpart{
+    display:flex;
+    justify-content: space-evenly;
+
+}
+.countinputbox{
+    display:flex;
+    justify-content: space-between;
+}
+.radiobutton{
+    margin-top: 50px;
+}
+.deleteele{
+    padding-top: 50px;
+    color:crimson;
+}
+.button{
+    margin-top: 50px;
+    background-color: black;
+    color:blanchedalmond;
+    border-radius:10px;
+}
+</style>
